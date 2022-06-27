7dcee08967fb303bf54d3f4527245ce4c73e6828
Ye Xinkang <yexinkang1234@gmail.com>
2022-06-27 22:49:08 +0800

    api changes

--- script.js
+++ script.js
@@ -9,30 +9,43 @@ techs.addEventListener("click", (e) => {
         selected.innerText = target.alt;
         imgselected.src = target.src;
         for (let item of techs.children) {
-            item.classList.remove('selected');
+            item.classList.remove("selected");
         }
-        target.classList.add('selected');
+        target.classList.add("selected");
     }
 });
 function formatAMPM(date) {
     let hours = date.getHours();
     let minutes = date.getMinutes();
-    let ampm = hours >= 12 ? 'pm' : 'am';
+    let ampm = hours >= 12 ? "pm" : "am";
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    let strTime = hours + ':' + minutes + ampm;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    let strTime = hours + ":" + minutes + ampm;
     return strTime;
 }
 function process(data) {
     let counter = 0;
-    const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    data.forEach(d => {
+    const month = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ];
+    data.forEach((d) => {
         let logo = d.committer.avatar_url;
         let date = new Date(d.commit.committer.date);
         let message = d.commit.message;
-        let commit = document.createElement('div');
-        commit.classList.add('github-commit');
+        let commit = document.createElement("div");
+        commit.classList.add("github-commit");
         commit.innerHTML = `
         <img src="${logo}" alt="${message}" class="profilepic">
             <div class="commit-info">
@@ -48,12 +61,7 @@ function process(data) {
 }
 function generateData() {
     let url = "https://api.github.com/repos/xk1234/xk1234.github.io/commits";
-    let options = {
-        headers: {
-            Authorization: "token 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"
-        }
-    };
-    fetch(url, options)
+    fetch(url)
         .then((response) => {
         return response.json();
     })
--- script.ts
+++ script.ts
@@ -6,61 +6,71 @@ const github: HTMLDivElement = document.querySelector(".github-content")!;
 techs.addEventListener("click", (e) => {
   let target = e.target! as HTMLImageElement;
   if ((<HTMLImageElement>e.target!).tagName === "IMG") {
-    selected.innerText = target.alt;    
+    selected.innerText = target.alt;
     imgselected.src = target.src;
     for (let item of techs.children) {
-        item.classList.remove('selected')
+      item.classList.remove("selected");
     }
-    target.classList.add('selected');
+    target.classList.add("selected");
   }
 });
 
 function formatAMPM(date: Date) {
-    let hours = date.getHours();
-    let minutes: string|number = date.getMinutes();
-    let ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes;
-    let strTime = hours + ':' + minutes + ampm;
-    return strTime;
-  }
+  let hours = date.getHours();
+  let minutes: string | number = date.getMinutes();
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  let strTime = hours + ":" + minutes + ampm;
+  return strTime;
+}
 
 function process(data: any) {
-    let counter = 0;
-    const month: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+  let counter = 0;
+  const month: string[] = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-    data.forEach(d =>{
-        let logo: string = d.committer.avatar_url
-        let date = new Date(d.commit.committer.date)
-        let message = d.commit.message
+  data.forEach((d) => {
+    let logo: string = d.committer.avatar_url;
+    let date = new Date(d.commit.committer.date);
+    let message = d.commit.message;
 
-        let commit = document.createElement('div')
-        commit.classList.add('github-commit')
-        commit.innerHTML = `
+    let commit = document.createElement("div");
+    commit.classList.add("github-commit");
+    commit.innerHTML = `
         <img src="${logo}" alt="${message}" class="profilepic">
             <div class="commit-info">
               <p class="title">Personal Website</p>
-              <p>${message}<span class="badge">${month[date.getMonth()]} ${date.getDate()}</span><span class="badge">${formatAMPM(date)}</span></p>
+              <p>${message}<span class="badge">${
+      month[date.getMonth()]
+    } ${date.getDate()}</span><span class="badge">${formatAMPM(date)}</span></p>
             </div>
-        `
-        if (counter < 5) {
-            github.append(commit)
-        }
-        counter++;
-    })
-
+        `;
+    if (counter < 5) {
+      github.append(commit);
+    }
+    counter++;
+  });
 }
 
 function generateData() {
-  let url: string = "https://api.github.com/repos/xk1234/xk1234.github.io/commits";
-  let options = {
-      headers: {
-        Authorization: "token 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"
-      }
-  }
+  let url: string =
+    "https://api.github.com/repos/xk1234/xk1234.github.io/commits";
 
-  fetch(url, options)
+  fetch(url)
     .then((response) => {
       return response.json();
     })
