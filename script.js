b01906ba06a3add3c3702e4220bc621775350b51
Ye Xinkang <yexinkang1234@gmail.com>
2022-06-28 19:34:43 +0800

    added icon and tech description

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ const techs = document.querySelector(".tech-row");
 const selected = document.querySelector(".selected");
 const imgselected = document.querySelector(".imgselected");
 const github = document.querySelector(".github-content");
+const techdesc = {
+    TypeScript: "I started learning TypeScript in mid-2022 and am now able to write all my JS code in TS, preventing a substantial number of errors. I am familiar with both the TS syntax as well as TS debugging and TS configuration",
+    JavaScript: "I have a solid conceptual understanding of the key vanilla JS principles including closures, scopes, OOP in JS, DOM manipulation, and async JS",
+    GraphQL: "I understand how HTTP and its different components work and am able to use JS to modify headers and body. I have worked with both REST and GraphQL on the client side",
+    HTML: "I understand the majority of the commonly used HTML principles well and have some knowledge of web accessibility",
+    CSS: "I fundamentally understand the basic principles of CSS including the box model, responsiveness, etc, and have utilized these principles to make my projects responsive",
+    Figma: "I started using Figma in 2021 and have become proficient in most basic features, including components, auto-layouts, creating styles, etc",
+    Django: "I have a good understanding of how to use the Django web framework to build common backend services like authentication, request handling, form validation, etc",
+};
 techs.addEventListener("click", (e) => {
     let target = e.target;
     if (e.target.tagName === "IMG") {
+        let paradesc = document.querySelector('.tech-main p');
         selected.innerText = target.alt;
         imgselected.src = target.src;
         for (let item of techs.children) {
             item.classList.remove("selected");
         }
+        paradesc.innerText = techdesc[target.alt];
         target.classList.add("selected");
     }
 });
--- script.ts
+++ script.ts
@@ -3,14 +3,31 @@ const selected: HTMLSpanElement = document.querySelector(".selected")!;
 const imgselected: HTMLImageElement = document.querySelector(".imgselected")!;
 const github: HTMLDivElement = document.querySelector(".github-content")!;
 
+interface alltech {
+  [tech: string]: string;
+}
+
+const techdesc = {
+  TypeScript:
+    "I started learning TypeScript in mid-2022 and am now able to write all my JS code in TS, preventing a substantial number of errors. I am familiar with both the TS syntax as well as TS debugging and TS configuration",
+  JavaScript: "I have a solid conceptual understanding of the key vanilla JS principles including closures, scopes, OOP in JS, DOM manipulation, and async JS",
+  GraphQL: "I understand how HTTP and its different components work and am able to use JS to modify headers and body. I have worked with both REST and GraphQL on the client side",
+  HTML: "I understand the majority of the commonly used HTML principles well and have some knowledge of web accessibility",
+  CSS: "I fundamentally understand the basic principles of CSS including the box model, responsiveness, etc, and have utilized these principles to make my projects responsive",
+  Figma: "I started using Figma in 2021 and have become proficient in most basic features, including components, auto-layouts, creating styles, etc",
+  Django: "I have a good understanding of how to use the Django web framework to build common backend services like authentication, request handling, form validation, etc",
+};
+
 techs.addEventListener("click", (e) => {
   let target = e.target! as HTMLImageElement;
   if ((<HTMLImageElement>e.target!).tagName === "IMG") {
+    let paradesc: HTMLParagraphElement = document.querySelector('.tech-main p')!;
     selected.innerText = target.alt;
     imgselected.src = target.src;
     for (let item of techs.children) {
       item.classList.remove("selected");
     }
+    paradesc.innerText = techdesc[target.alt];
     target.classList.add("selected");
   }
 });
